64c7e4d369aadfd86c7a28d15b030fb87b002560
Harsh <hs8961524@gmail.com>
2023-08-31 23:43:12 +0530

    co

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const express = require('express');
 const port = 8000;
 const app = express();
 
-//use express router
-app.use('/' , require('./routes/index'));
-
-//setting up our view engine
-app.set('view engine' , 'ejs');
-app.set('views' , './views');
-
 app.listen(port , function(err){
     if(err){
         console.log(`Error in starting the server: ${err}`);
