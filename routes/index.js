17a0bb11bbf0844c6247d71c659543c5c8d705eb
Harsh <hs8961524@gmail.com>
2023-09-16 14:30:55 +0530

    create route controller and save post to DB

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,7 @@ router.get('/' , homecontroller.home);
 //when any user request comes it shifts control to users route
 router.use('/users' , require('./users'));
 
+//when any posts request comes it shift control to post route
+router.use('/post', require('./post'))
+
 module.exports = router;
